cadb0eab26a7180bbe76658d4df9a3031cef3ca6
Ajit <ajitstha16@gmail.com>
2023-10-09 08:40:14 +0545

    created User model in users app

--- Soch/users/models.py
+++ Soch/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    
+    MALE = "m"
+    FEMALE = "f"
+    OTHER = "o"
+    
+    GENDER_CHOICES =(
+        (MALE, "Male"),
+        (FEMALE, "Female"),
+        (OTHER, "other"),
+    )
+
+    
+    id = models.PositiveBigIntegerField(primary_key=True)
+    profile_pic = models.ImageField(upload_to="image/users", blank=True, null=True)
+    phone = models.CharField(max_length=10, unique=True)
+    Address = models.CharField(max_length=100, blank=True, null=True)
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+    )
+    dob = models.DateField()
