35641c0277042104f2466e885efd96361172151c
Ajit <ajitstha16@gmail.com>
2023-10-09 09:30:21 +0545

    setup media and static

--- Soch/users/models.py
+++ Soch/users/models.py
@@ -3,24 +3,26 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    
     MALE = "m"
     FEMALE = "f"
     OTHER = "o"
-    
-    GENDER_CHOICES =(
+
+    GENDER_CHOICES = (
         (MALE, "Male"),
         (FEMALE, "Female"),
         (OTHER, "other"),
     )
 
-    
-    id = models.PositiveBigIntegerField(primary_key=True)
+    USERNAME_FIELD = "email"
+
     profile_pic = models.ImageField(upload_to="image/users", blank=True, null=True)
     phone = models.CharField(max_length=10, unique=True)
-    Address = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
     gender = models.CharField(
         max_length=1,
         choices=GENDER_CHOICES,
     )
     dob = models.DateField()
+
+    def __str__(self) -> str:
+        return self.username
