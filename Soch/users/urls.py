7bd8c536f91280e3c76ff21af12ae8de3f7399b4
Niraj Pandey <neeraz.pandey2058@gmail.com>
2023-10-13 12:01:02 +0545

    Signup templates added

--- Soch/users/urls.py
+++ Soch/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.loginpage, name='signup'),
+    path('signup', views.signuppage, name='signup')
 ]
--- Soch/users/views.py
+++ Soch/users/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
-def index(request):
+def loginpage(request):
     return render(request, 'User/login.html')
+
+def signuppage(request):
+    return render(request, 'User/signup.html')
 
 def handlelogin(request):
     pass
+
+def handlesignup(request):
+    if request.method=="POST":
+        email = request.POST('email')
+        username = request.POST('username')
+        first_name = request.POST('first_name')
+        last_name = request.POST('last_name')
+        password1 = request.POST('password1')
+        password2 = request.POST('password2')
+        profile_pic = request.POST('profile_pic')
+        gender = request.POST('gender')
+        dob = request.POST('dob')
+        phone = request.POST('phone')
+        address = request.POST('address')
