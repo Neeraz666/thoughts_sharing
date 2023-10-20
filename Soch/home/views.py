6fe5c5ace72e219fb5cb8858c5fc5acd501fcaa1
Niraj Pandey <neeraz.pandey2058@gmail.com>
2023-10-20 10:23:22 +0545

    urls changed

--- Soch/home/views.py
+++ Soch/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from users.models import *
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, 'home/index.html')
+    user = request.user
+    return render(request, 'home/index.html', {'user': user})
--- Soch/users/urls.py
+++ Soch/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.loginpage, name="login"),
+    path("", views.loginpage, name="login"),
     path("signup/", views.signuppage, name="signup"),
     path("handlesignup", views.handlesignup, name="handlesignup"),
     path('handlelogin', views.handlelogin, name="handlelogin"),
